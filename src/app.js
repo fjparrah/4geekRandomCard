f66d8da2c4f6c9ced0ece89defd8d84e1e8c7359
fjparrah <48214359+fjparrah@users.noreply.github.com>
2023-12-02 15:06:22 +0000

    actualizado 2

--- src/app.js
+++ src/app.js
@@ -1,27 +1,3 @@
-/* eslint-disable */
-// import "bootstrap";
-// import "./style.css";
-
-// import "./assets/img/rigo-baby.jpg";
-// import "./assets/img/4geeks.ico";
-
-// window.onload = function() {
-//   //write your code here
-//   console.log("Hello Rigo from the console!");
-// };
-
-// function cambiaNumero(){
-
-//     let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-
-//     let numeroAleatorio = numeros[Math.floor(Math.random() * numeros.length)];
-
-//     let parrafo = document.getElementById('numero');
-
-//     parrafo.textContent = `${numeroAleatorio}`;
-
-// }
-
 function obtenerNumeroAleatorio() {
   let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "Q", "K"];
   let numeroAl = numeros[Math.floor(Math.random() * numeros.length)];
