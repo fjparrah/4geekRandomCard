fc8109d58a4d7d897baf782b6756243126f32f7d
fjparrah <48214359+fjparrah@users.noreply.github.com>
2023-12-02 16:37:01 +0000

    finalizado

--- src/app.js
+++ src/app.js
@@ -1,18 +1,33 @@
+function obtenersignoAleatorio() {
+  let picas = [
+    "https://images.emojiterra.com/google/noto-emoji/unicode-15.1/color/svg/2665.svg",
+    "https://images.emojiterra.com/google/noto-emoji/unicode-15.1/color/svg/2660.svg",
+    "https://images.emojiterra.com/google/noto-emoji/unicode-15.1/color/svg/2663.svg",
+    "https://images.emojiterra.com/google/noto-emoji/unicode-15.1/color/svg/2666.svg",
+  ];
+  let picaElegida = picas[Math.floor(Math.random() * picas.length)];
+
+  return picaElegida;
+}
+
 function obtenerNumeroAleatorio() {
   let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "Q", "K"];
   let numeroAl = numeros[Math.floor(Math.random() * numeros.length)];
 
   return numeroAl;
-
-  // return Math.floor(Math.random() * 12) + 1;
 }
 
 function manejarClick() {
   let numeroAleatorio = obtenerNumeroAleatorio();
   let parrafo = document.getElementById("numero");
   parrafo.innerHTML = numeroAleatorio;
-  // parrafo.textContent = numeroAleatorio;
-  //
+
+  //signo
+  let signoAleatorio = obtenersignoAleatorio();
+  let signo = document.getElementById("imagen");
+  let signo1 = document.getElementById("imagen1");
+  signo.src = signoAleatorio;
+  signo1.src = signoAleatorio;
 }
 
 // Agrega un listener al evento 'click' en la ventana (pantalla)
